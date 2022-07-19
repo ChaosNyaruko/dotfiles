5095149ffde132ff1fbaec162ffd0b645f97e2a0
ChaosNyaruko <cabbageparadise@gmail.com>
2022-07-19 10:59:53 +0800

    multiple line string demo

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@ local api = vim.api
 api.nvim_echo({{"hello, I'm lua init file!",""}}, false, {})
 api.nvim_echo({{"hello, I'm lua init file! - s!",""}}, true, {})
 -- print "hello, neovim lua!"
+--
+
+cmd = [[something like
+go's ``(backticks surrounded strings),
+you can see it! space ]]
+print(cmd)
