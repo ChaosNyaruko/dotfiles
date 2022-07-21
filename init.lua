4626111b6786867a001c4507869dc2997012cb94
ChaosNyaruko <cabbageparadise@gmail.com>
2022-07-21 22:31:06 +0800

    try some lua settings

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
 local api = vim.api
 api.nvim_echo({{"hello, I'm lua init file!",""}}, false, {})
-api.nvim_echo({{"hello, I'm lua init file! - s!",""}}, true, {})
--- print "hello, neovim lua!"
+--api.nvim_echo({{"hello, I'm lua init file! - s!",""}}, true, {})
+---- print "hello, neovim lua!"
+----
 --
-
-cmd = [[something like
-go's ``(backticks surrounded strings),
-you can see it! space ]]
-print(cmd)
+--cmd = [[something like
+--go's ``(backticks surrounded strings),
+--you can see it! space ]]
+--print(cmd)
+--
+-- :help api.txt
+api.nvim_win_set_option(0, "number", true)
+api.nvim_win_set_option(0, "relativenumber",true)
