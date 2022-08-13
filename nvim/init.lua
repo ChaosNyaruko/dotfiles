3bf326b5dc45bb3ff95295c4335b9fe74d0f1691
ChaosNyaruko <cabbageparadise@gmail.com>
2022-08-14 01:46:11 +0800

    2022/08/14 01:46:07

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,19 @@
 -- from scratch
-require ("base")
-require ("maps")
-require ("plugins")
+require("base")
+require("maps")
+require("plugins")
 
 local has = function(x)
-	return vim.fn.has(x) == 1
+    return vim.fn.has(x) == 1
 end
 
 local has_clipboard = has "clipboard"
 local has_unnamedplus = has "unnamedplus"
 
 if has_clipboard then
-	if has_unnamedplus then
-		vim.opt.clipboard = {'unnamed', 'unnamedplus'}
-	else
-		vim.opt.clipboard = {'unnamed'}
-	end
+    if has_unnamedplus then
+        vim.opt.clipboard = { 'unnamed', 'unnamedplus' }
+    else
+        vim.opt.clipboard = { 'unnamed' }
+    end
 end
