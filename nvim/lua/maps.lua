af4c1d4262789476c0fcee61e5eeafdd920bfd7c
ChaosNyaruko <cabbageparadise@gmail.com>
2022-08-16 17:33:21 +0800

    set map for terminal switching back to normal mode

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,12 +1,14 @@
 local keymap = vim.keymap
 
-nopts = { noremap = true }
+local nopts = { noremap = true }
 
 keymap.set('v', '<', '<gv', nopts)
 keymap.set('v', '>', '>gv', nopts)
 
 keymap.set('n', '<leader>sv', ':source $MYVIMRC<cr>', nopts)
 keymap.set('n', '<leader>ev', ':vsp $MYVIMRC<cr>', nopts)
+
+keymap.set('t', '<esc>', '<c-\\><c-n>', nopts)
 
 function _G.ToggleEmacs()
     if vim.g.emacs_mapping == 1 then
