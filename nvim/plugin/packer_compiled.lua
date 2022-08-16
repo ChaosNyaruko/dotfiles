e03457826b2c5d3aa64849df547dd183298876d8
ChaosNyaruko <cabbageparadise@gmail.com>
2022-08-16 10:35:53 +0800

    2022/08/16 10:35:49

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "/Users/bytedance/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/Users/bytedance/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
   ["nvim-web-devicons"] = {
     loaded = true,
     path = "/Users/bytedance/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
