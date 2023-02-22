09302abfa7737c68400d7ffca7a511a880586191
ChaosNyaruko <cabbageparadise@gmail.com>
2023-02-22 11:44:02 +0800

    bootstrapping for packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,10 +1,21 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[ packadd packer.nvim ]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local status, packer = pcall(require, 'packer')
 if (not status) then
     print("Packer is not installed")
     return
 end
-
-vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
     use 'wbthomason/packer.nvim'
@@ -43,4 +54,14 @@ packer.startup(function(use)
 
     -- use { 'junegunn/fzf', run = ":call fzf#install()" }
     -- use { 'junegunn/fzf.vim' }
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
