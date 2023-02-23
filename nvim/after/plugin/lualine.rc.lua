85cbee8e5fd9fb772df3f40a70f32f038fdd1a4a
ChaosNyaruko <cabbageparadise@gmail.com>
2023-02-23 14:52:03 +0800

    lualine config: relative path in filename

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -23,9 +23,12 @@ line.setup {
         }
     },
     sections = {
-        lualine_a = { 'mode' },
+        lualine_a = {
+            'mode',
+        },
+
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
