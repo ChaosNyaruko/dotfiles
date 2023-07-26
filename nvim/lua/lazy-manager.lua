5eb68eda7313c812d3a33d4a6d12de4cd957bdac
ChaosNyaruko <cabbageparadise@gmail.com>
2023-07-26 20:09:46 +0800

    disable which key

--- nvim/lua/lazy-manager.lua
+++ nvim/lua/lazy-manager.lua
@@ -57,6 +57,7 @@ require("lazy").setup({
     {
         "folke/which-key.nvim",
         event = "VeryLazy",
+        enabled = false,
         init = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
