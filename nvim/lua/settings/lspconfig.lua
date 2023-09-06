ce41681940506cebafd61c1f6ef91606fe1c7aa4
ChaosNyaruko <cabbageparadise@gmail.com>
2023-09-06 20:59:58 +0800

    bugfix: global mapping should not be "buffer"ed, otherwise it won't take effect if you switch the buffer

--- nvim/lua/settings/lspconfig.lua
+++ nvim/lua/settings/lspconfig.lua
@@ -7,7 +7,7 @@ vim.diagnostic.config({ virtual_text = true })
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { buffer = true, noremap = true, silent = true, desc = "mappings in lspconfig global" }
+local opts = { buffer = false, noremap = true, silent = true, desc = "mappings in lspconfig global" }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
