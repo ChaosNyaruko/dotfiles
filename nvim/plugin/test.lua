08ebc35e8ee38dc701ea7e4534d7b7ff01700922
ChaosNyaruko <cabbageparadise@gmail.com>
2023-10-12 17:13:27 +0800

    fix <leader>t

--- nvim/plugin/test.lua
+++ nvim/plugin/test.lua
@@ -1,6 +1,7 @@
 -- print("plugin/test.lua loaded")
 vim.keymap.set("n", "<leader>t", function()
+    local cwd = vim.fn.expand('%:p:h')
     vim.cmd.tabnew()
-    vim.fn.termopen(vim.o.shell, { cwd = vim.fn.expand('%:p:h') })
+    vim.fn.termopen(vim.o.shell, { cwd = cwd })
     vim.cmd [[startinsert]]
 end)
