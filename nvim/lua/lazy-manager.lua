41b033a791da96eabe125072a93daa5d1ba72300
ChaosNyaruko <cabbageparadise@gmail.com>
2024-01-07 13:13:55 +0800

    diable codeium on rust

--- nvim/lua/lazy-manager.lua
+++ nvim/lua/lazy-manager.lua
@@ -284,6 +284,10 @@ local plugins = {
             end
         end,
         event = "VeryLazy",
+        init = function ()
+            vim.g.codeium_enabled = true
+            vim.g.codeium_filetypes = { python = true, go = true, rust = false }
+        end,
         config = function()
             -- Change '<C-g>' here to any keycode you like.
             vim.keymap.set('i', '<tab>', function() return vim.fn['codeium#Accept']() end, { expr = true })
@@ -291,8 +295,6 @@ local plugins = {
                 { expr = true, desc = "codedium#CycleCompletions" })
             vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
             vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-            vim.g.codeium_enabled = true
-            vim.g.codeium_filetypes = { python = true, go = true }
         end
     },
     {
