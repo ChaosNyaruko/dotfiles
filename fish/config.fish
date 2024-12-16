7502e87831e4cc50b738ce22ac8fa3e4a273e242
ChaosNyaruko <cabbageparadise@gmail.com>
2024-12-16 14:51:11 +0800

    fish abbr gctt -> easier commit for daily records

--- fish/config.fish
+++ fish/config.fish
@@ -28,6 +28,7 @@ abbr -a gcm git commit -m
 abbr -a gd git diff
 abbr -a gaa git add --all
 abbr -a diff nvim -d
+abbr -a gctt git commit -m \'$(gdate +'%Y-%m-%d %H:%M:%S')\'
 
 set -gx FZF_DEFAULT_OPTS "--preview-window 'right:57%' --preview 'bat --style=numbers --line-range :300 {}' --bind ctrl-y:preview-up,ctrl-e:preview-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down,ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,shift-up:preview-top,shift-down:preview-bottom,alt-up:half-page-up,alt-down:half-page-down" 
 set -gx FZF_DEFAULT_COMMAND "fd --hidden --type f" 
