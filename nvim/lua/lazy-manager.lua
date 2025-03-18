5bc51a76a62c859ba5209be685e73f351571f2b8
ChaosNyaruko <cabbageparadise@gmail.com>
2025-03-18 14:55:23 +0800

    try img-clip plugin in neovim

--- nvim/lua/lazy-manager.lua
+++ nvim/lua/lazy-manager.lua
@@ -51,6 +51,18 @@ local plugins = {
         end
     },
     {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add options here
+            -- or leave it empty to use the default settings
+        },
+        keys = {
+            -- suggested keymap
+            { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+        },
+    },
+    {
         enabled = true,
         "folke/snacks.nvim",
         priority = 1000,
